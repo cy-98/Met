374e66657d3d121522a5de56912083931bef6ac3
CaiyueDamowang <www.caiyue@qq.com>
2019-05-14 15:10:11 +0800

    添加了动态页面和个人页面

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ App({
     onLaunch(options) {
         this.appIMDelegate = new AppIMDelegate(this);
         this.appIMDelegate.onLaunch(options);
+
+        wx.getSystemInfo({
+          success: e => {
+            this.globalData.StatusBar = e.statusBarHeight;
+            let custom = wx.getMenuButtonBoundingClientRect();
+            this.globalData.Custom = custom;
+            this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+          }
+        });
     },
     onHide() {
         this.appIMDelegate.onHide();
