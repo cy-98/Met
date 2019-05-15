a1c5338eebfe670feb057747766ba54903c74a12
yuan <xingyuan1998@163.com>
2019-05-15 11:34:17 +0800

    动态添加页面

--- pages/dynamic/add/add.js
+++ pages/dynamic/add/add.js
@@ -1,20 +1,15 @@
-// pages/dynamic/list.js
+// pages/dynamic/add/add.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    TabCur: 0,
-    scrollLeft: 0
+    imgList: [],
+    index: null,
+    picker: ['表白墙', '吐槽墙', '随便说说'],
   },
 
-  tabSelect(e) {
-    this.setData({
-      TabCur: e.currentTarget.dataset.id,
-      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
-    })
-  },
   /**
    * 生命周期函数--监听页面加载
    */
