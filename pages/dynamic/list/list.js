201a1f49d82b884df8348ccdc81c478609c6bff6
yuan <xingyuan1998@163.com>
2019-05-30 17:13:38 +0800

    添加了动态添加跳转按钮

--- pages/dynamic/list/list.js
+++ pages/dynamic/list/list.js
@@ -17,18 +17,24 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this
     wx.getSystemInfo({
-      success: function (res) {
+      success: function(res) {
         that.setData({
           clientHeight: res.windowHeight
         });
       }
     });
   },
+  addDynamic: function() {
+    console.info("add dynamic");
+    wx.navigateTo({
+      url: '/pages/dynamic/add/add',
+    })
+  },
 
-  swiperchange: function (e) {
+  swiperchange: function(e) {
     var that = this
     console.log(e.detail.current)
     that.setData({
@@ -39,49 +45,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
