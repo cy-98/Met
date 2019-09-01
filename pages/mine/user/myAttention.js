c48de47be053654ea7e1faaca6be0699ee0c6c9f
CaiyueDamowang <www.caiyue@qq.com>
2019-09-01 22:52:54 +0800

    关注别人

--- pages/mine/user/myAttention.js
+++ pages/mine/user/myAttention.js
@@ -7,16 +7,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    users:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(this.data.users.length)
     network.getUserFollow({
       success:(res)=>{
-        console.log(res)
+        this.setData({
+          users:res.data
+        })
       },
       fail:()=>{
 
