a79f0ea07cc0ff1c11a39da043136b78e57816a1
yuan <xingyuan1998@163.com>
2019-09-06 23:25:11 +0800

    添加了搜索用户

--- pages/friends/friends.js
+++ pages/friends/friends.js
@@ -5,21 +5,16 @@ Page({
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
     hidden: true,
-    inputValue:''
+    inputValue:'',
+    users:[]
   },
-  onLoad() {
-    let list = [];
-    for (let i = 0; i < 26; i++) {
-      list[i] = String.fromCharCode(65 + i)
-    }
-    this.setData({
-      list: list,
-      listCur: list[0]
-    });
+  onLoad:function() {
 
     network.getUserFollower({
       success:res => {
-
+        this.setData({
+          users:res.data
+        })
       },
       fail: res => {
 
@@ -27,84 +22,26 @@ Page({
     })
 
   },
-  onReady() {
-    let that = this;
-    wx.createSelectorQuery().select('.indexBar-box').boundingClientRect(function (res) {
-      that.setData({
-        boxTop: res.top
-      })
-    }).exec();
-    wx.createSelectorQuery().select('.indexes').boundingClientRect(function (res) {
-      that.setData({
-        barTop: res.top
-      })
-    }).exec()
-  },
-  //获取文字信息
-  getCur(e) {
-    this.setData({
-      hidden: false,
-      listCur: this.data.list[e.target.id],
+  clickUser:function(e){
+    wx.navigateTo({
+      url: '/pages/mine/user/info?id=' + e.currentTarget.dataset.user.id,
     })
   },
 
-  setCur(e) {
-    this.setData({
-      hidden: true,
-      listCur: this.data.listCur
-    })
-  },
-  //滑动选择Item
-  tMove(e) {
-    let y = e.touches[0].clientY,
-      offsettop = this.data.boxTop,
-      that = this;
-    //判断选择区域,只有在选择区才会生效
-    if (y > offsettop) {
-      let num = parseInt((y - offsettop) / 20);
-      this.setData({
-        listCur: that.data.list[num]
-      })
-    };
-  },
-
-  //触发全部开始选择
-  tStart() {
-    this.setData({
-      hidden: false
-    })
-  },
-
-  //触发结束选择
-  tEnd() {
-    this.setData({
-      hidden: true,
-      listCurID: this.data.listCur
-    })
-  },
-  indexSelect(e) {
-    let that = this;
-    let barHeight = this.data.barHeight;
-    let list = this.data.list;
-    let scrollY = Math.ceil(list.length * e.detail.y / barHeight);
-    for (let i = 0; i < list.length; i++) {
-      if (scrollY < i + 1) {
-        that.setData({
-          listCur: list[i],
-          movableY: i * 20
-        })
-        return false
-      }
-    }
-  },
   searchUser:function(e){
+    if(this.data.inputValue === ""){
+      wx.showToast({
+        title: '不能为空',
+      });
+      return;
+    }
     network.searchUser({
-      param : this.data.inputValue,
+      params : this.data.inputValue,
       formId : e.detail.formId,
       success:(res)=>{
-        wx.navigateTo({
-          url: '',//+用户id
-          })
+        this.setData({
+          users:res.data.data
+        })
       },
       fail:()=>{
         //没有该用户
